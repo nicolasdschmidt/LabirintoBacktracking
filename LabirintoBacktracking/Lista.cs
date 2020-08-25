ecf89baa3cfca076d4289bd3dd4c26e931011e63
Nícolas D. Schmidt <nicolasdschmidt@gmail.com>
2020-08-25 09:50:36 -0300

    Adicionar classes Lista e Pilha

---
+++ LabirintoBacktracking/Lista.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabirintoBacktracking
+{
+    class Lista<X>
+    {
+        protected class No
+        {
+            public X Info { get; set; }
+            public No Ante { get; set; }
+            public No Prox { get; set; }
+
+            public No (X i, No a, No p)
+            {
+                Info = i;
+                Ante = a;
+                Prox = p;
+            }
+
+            public No(X i)
+            {
+                Info = i;
+                Ante = null;
+                Prox = null;
+            }
+        }
+
+        protected No primeiro, ultimo;
+
+        public Lista()
+        {
+            primeiro = ultimo = null;
+        }
+
+        public void InserirNoFim(X i)
+        {
+            if (i == null)
+                throw new Exception("Informação ausente");
+
+            if (ultimo == null)
+            {
+                ultimo = new No(i);
+                primeiro = ultimo;
+            } else
+            {
+                No novo = new No(i);
+                novo.Ante = ultimo;
+                ultimo.Prox = novo;
+                ultimo = novo;
+            }
+        }
+
+        public void InserirNoInicio(X i)
+        {
+            if (i == null)
+                throw new Exception("Informação ausente");
+
+            if (primeiro == null)
+            {
+                primeiro = new No(i);
+                primeiro = ultimo;
+            }
+            else
+            {
+                No novo = new No(i);
+                novo.Prox = primeiro;
+                primeiro.Ante = novo;
+                primeiro = novo;
+            }
+        }
+
+        public void RemoverDoInicio()
+        {
+            if (primeiro == null)
+                throw new Exception("Nada a remover");
+
+            if (primeiro == ultimo) 
+            {
+                primeiro = null;
+                ultimo = null;
+                return;
+            }
+
+            primeiro = primeiro.Prox;
+            primeiro.Ante = null;
+        }
+
+        public void RemoverDoFim()
+        {
+            if (ultimo == null)
+                throw new Exception("Nada a remover");
+
+            if (primeiro == ultimo)
+            {
+                primeiro = null;
+                ultimo = null;
+                return;
+            }
+
+            ultimo = ultimo.Ante;
+            ultimo.Prox = null;
+        }
+
+        public X GetInicio()
+        {
+            if (primeiro == null)
+                throw new Exception("Nada a obter");
+
+            return primeiro.Info;
+        }
+
+        public X GetFim()
+        {
+            if (ultimo == null)
+                throw new Exception("Nada a obter");
+
+            return ultimo.Info;
+        }
+
+        public bool Vazia()
+        {
+            return primeiro == null;
+        }
+
+        public int GetQtd()
+        {
+            if (primeiro == null)
+                return 0;
+
+            int ret = 0;
+
+            No atual = primeiro;
+
+            while (atual != null)
+            {
+                ret++;
+                atual = atual.Prox;
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ LabirintoBacktracking/Pilha.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabirintoBacktracking
+{
+    class Pilha<X>
+    {
+        Lista<X> lista = new Lista<X>();
+
+        public Pilha() {}
+
+        public bool Vazia()
+        {
+            return lista.Vazia();
+        }
+
+        public X Espiar()
+        {
+            return lista.GetFim();
+        }
+
+        public X Desempilhar()
+        {
+            X ret = lista.GetFim();
+            lista.RemoverDoFim();
+            return ret;
+        }
+
+        public void Adicionar(X i)
+        {
+            lista.InserirNoFim(i);
+        }
+    }
+}
