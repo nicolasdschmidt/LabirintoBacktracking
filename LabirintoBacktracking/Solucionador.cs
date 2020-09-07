33791e7f7ce7fea2b447840b6c4073d0be84d810
Henrique <50873208+henriqueAfonso@users.noreply.github.com>
2020-09-07 11:29:32 -0300

    metodos para achar caminho

--- LabirintoBacktracking/Solucionador.cs
+++ LabirintoBacktracking/Solucionador.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,12 +8,78 @@ namespace LabirintoBacktracking
 {
     class Solucionador
     {
-        private Labirinto lab;
-        private Pilha<Movimento> passos;
+        private Labirinto lab;// o labirinto
+        private Pilha<Movimento> passos;// os movimentos de uma solução
+        private List<Pilha<Movimento>> solucoes;// uma lista com todas as soluções possiveis
+
+        private int colunaAtual;//as coordenadas 
+        private int linhaAtual;//atuais
+
+        private bool achouASaida;//
 
         // TODO: Construtor que aceita Labirinto e guarda
+        public Solucionador(Labirinto labirinto)
+        {
+            this.lab = labirinto;   //recebe o labirinto
+
+            this.passos = new Pilha<Movimento>(); //instancia os movimentos
+
+            colunaAtual = 1;    //define as coordenadas
+            linhaAtual = 1;     //iniciais do labirinto
+
+            achouASaida = false;//define que a saida ainda não foi encontrada
+        }
+
+       
+        public int GetColunaAtual() { return colunaAtual; }
+
+        public int GetLinhaAtual() { return linhaAtual; }
+
+        public bool GetAchouSaida() { return achouASaida; }
+
+
         /* TODO: Algoritmo de resolução por passos, um método Passo vai executar apenas 1 passo de tentativa de solução
          * (tem que ser em passos pra conseguirmos mostrar a solução sendo processada no DataGridView)
          */
+        public bool DarUmPasso()//tenta dar um passo. se conseguir retorna true e, se não, retorna false
+        {
+
+            for (int i = -1; i<=1; i++)
+            {
+                for (int o = -1; o <= 1; o++)
+                {
+                    //se a posição que ele achou não for uma parede e 
+                    //nem uma posição ja explorada do labirinto
+                    
+                    if (this.lab.Dados[linhaAtual + i, colunaAtual + o] != '#' && this.lab.Dados[linhaAtual + i, colunaAtual + o] != 'p')
+                    {
+                        this.linhaAtual += i;   //define as novas
+                        this.colunaAtual += o;  //coordenadas
+
+                        if (this.lab.Dados[linhaAtual, colunaAtual] == 'S')
+                            this.achouASaida = true;
+
+
+                        this.lab.Dados[linhaAtual, colunaAtual] = 'p'; //marca que já passou                      
+
+                        this.passos.Adicionar(new Movimento(i, o)); //adiciona o movimento que acabou de ser feito
+
+                        return true; //retorna que achou caminho
+                        
+                    }                
+                }
+            }
+            return false;
+        }
+
+        public void VoltarUmPasso()
+        {
+            Movimento aux = passos.Desempilhar();
+            this.linhaAtual += -aux.AlteradorLinha;
+            this.colunaAtual += -aux.AlteradorColuna;
+        }
+
+        
     }
 }
+
